3fafb7cc48bc379b33e482df9fdaf3a64cf20c96
Oskar Niesen <oskarniesen1@gmail.com>
2025-03-18 13:24:07 -0500

    Added action and dialog

--- src/main/kotlin/org/example/gitcommitrename/CommitRename.kt
+++ src/main/kotlin/org/example/gitcommitrename/CommitRename.kt
@@ -2,12 +2,34 @@ package org.example.gitcommitrename
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.components.JBTextField
+import java.awt.BorderLayout
+import javax.swing.JComponent
+import javax.swing.JPanel
 
-public class RenameCommit : AnAction() {
+class RenameCommit : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        val editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        RenameCommitDialog().show()
+    }
+
+}
+
+class RenameCommitDialog() : DialogWrapper(true) {
+    init {
+        title = "Rename last commit"
+        init()
+    }
+    override fun createCenterPanel(): JComponent {
+        val renameDialog = JPanel(BorderLayout())
+        val label = JBTextField("last commit name")
+//        label.preferredSize = Dimension(100, 100)
+        renameDialog.add(label)
+        return renameDialog
+    }
+
+    override fun doOKAction() {
+        super.doOKAction()
     }
 
 }
