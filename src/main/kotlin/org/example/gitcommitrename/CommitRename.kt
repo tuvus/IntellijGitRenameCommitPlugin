41f4b038d9adcbd59c29c47efeae6c8023ff1f03
Oskar Niesen <oskarniesen1@gmail.com>
2025-03-18 17:17:56 -0500

    Rename Dialog now shows previous commit message

--- src/main/kotlin/org/example/gitcommitrename/CommitRename.kt
+++ src/main/kotlin/org/example/gitcommitrename/CommitRename.kt
@@ -2,33 +2,62 @@ package org.example.gitcommitrename
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.components.JBTextField
+import com.intellij.ui.JBColor
+import com.intellij.ui.components.JBTextArea
+import com.intellij.util.ui.JBUI
 import java.awt.BorderLayout
+import java.awt.Dimension
 import javax.swing.JComponent
+import javax.swing.JLabel
 import javax.swing.JPanel
+import git4idea.commands.Git
+import git4idea.commands.GitCommand
+import git4idea.commands.GitLineHandler
+import git4idea.repo.GitRepositoryManager
+import kotlin.concurrent.thread
 
 class RenameCommit : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        RenameCommitDialog().show()
+        val project = e.project
+            ?: throw Exception("Trying to change the name of a commit without an oppen project!")
+        val root = GitRepositoryManager.getInstance(project).repositories.firstOrNull()?.root
+            ?: throw Exception("Trying to change the name of a commit without the repository!")
+        val handler = GitLineHandler(project, root, GitCommand.LOG)
+        var previousText = "Failed to load"
+        thread {
+            previousText = Git.getInstance().runCommand(handler).getOutputOrThrow()
+            var startingIndex = previousText.indexOf('\n', previousText.indexOf("Date:"))
+            startingIndex = previousText.indexOf('\n', startingIndex + 1)
+            previousText = previousText.substring(startingIndex, previousText.indexOf('\n', startingIndex + 1)).trim()
+        }.join()
+        RenameCommitDialog(previousText, project).show()
     }
 
 }
 
-class RenameCommitDialog() : DialogWrapper(true) {
+class RenameCommitDialog(val previousCommitText: String, project: Project) : DialogWrapper(true) {
+    lateinit var textArea:JBTextArea
     init {
         title = "Rename last commit"
         init()
     }
     override fun createCenterPanel(): JComponent {
-        val renameDialog = JPanel(BorderLayout())
-        val label = JBTextField("last commit name")
-//        label.preferredSize = Dimension(100, 100)
-        renameDialog.add(label)
+        val layout = BorderLayout()
+        layout.vgap = 10
+        val renameDialog = JPanel(layout)
+        renameDialog.preferredSize = Dimension(400, 200)
+        renameDialog.add(JLabel("Rename commit:"), BorderLayout.PAGE_START)
+
+        textArea = JBTextArea(previousCommitText)
+        textArea.border = JBUI.Borders.compound(JBUI.Borders.customLine(JBColor.GRAY, 1), JBUI.Borders.empty(3))
+        renameDialog.add(textArea, BorderLayout.CENTER)
         return renameDialog
     }
 
     override fun doOKAction() {
+        println(textArea.text)
         super.doOKAction()
     }
 
